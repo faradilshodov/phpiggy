6daed9e92c937f657fa02c0c89aa118a567a3097
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2024-10-20 10:20:45 -0400

    Continued working on form validation

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Framework;
 
 use Framework\Contracts\RuleInterface;
+use Framework\Exceptions\ValidationException;
 
 class Validator
 {
@@ -35,7 +36,7 @@ class Validator
         }
 
         if (count($errors)) {
-            dd($errors);
+            throw new ValidationException($errors);
         }
     }
 }
