9784f41d54dc3e102d8b8351d797a79b62cc1286
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-02-19 10:09:48 -0500

    Added route middleware to login, register and home pages

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -10,13 +10,17 @@ use App\Controllers\{
     AboutController, 
     AuthController
 };
+use App\Middleware\{
+    AuthRequiredMiddleware, 
+    GuestOnlyMiddleware
+};
 
 function registerRoutes(App $app)
 {
-    $app->get('/', [HomeController::class, 'home']);
+    $app->get('/', [HomeController::class, 'home'])->add(AuthRequiredMiddleware::class);
     $app->get('/about', [AboutController::class, 'about']);
-    $app->get('/register', [AuthController::class, 'registerView']);
-    $app->post('/register', [AuthController::class, 'register']);
-    $app->get('/login', [AuthController::class, 'loginView']);
-    $app->post('/login', [AuthController::class, 'login']);
+    $app->get('/register', [AuthController::class, 'registerView'])->add(GuestOnlyMiddleware::class);
+    $app->post('/register', [AuthController::class, 'register'])->add(GuestOnlyMiddleware::class);
+    $app->get('/login', [AuthController::class, 'loginView'])->add(GuestOnlyMiddleware::class);
+    $app->post('/login', [AuthController::class, 'login'])->add(GuestOnlyMiddleware::class);
 }
