d27523bbf7034723f6024e964345fd1490eacd32
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-02-26 17:01:07 -0500

    Added file validations

--- src/App/Services/ReceiptService.php
+++ src/App/Services/ReceiptService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Framework\Database;
+use Framework\Exceptions\ValidationException;
 
 class ReceiptService
 {
@@ -12,6 +13,35 @@ class ReceiptService
 
     public function validateFile(?array $file)
     {
-        dd($file);
+        if (!$file || $file['error'] !== UPLOAD_ERR_OK) {
+            throw new ValidationException([
+                'receipt' => ['Receipt file is required'],
+            ]);
+        }
+
+        $maxFileSizeMB = 3 * 1024 * 1024; // 3 MB
+
+        if($file['size'] > $maxFileSizeMB) {
+            throw new ValidationException([
+                'receipt' => ['Receipt file size exceeds the maximum limit of 3 MB'],
+            ]);
+        }
+
+        $originalFileName = $file['name'];
+
+        if(!preg_match('/^[A-za-z0-9\s._-]+$/', $originalFileName)) {
+            throw new ValidationException([
+                'receipt' => ['Invalid file name'],
+            ]);
+        }
+
+        $clientMimeType =  $file['type'];
+        $allowedMimeTypes = ['image/jpeg', 'image/png', 'application/pdf'];
+
+        if (!in_array($clientMimeType, $allowedMimeTypes)) {
+            throw new ValidationException([
+                'receipt' => ['Invalid file type. Only JPG, PNG, and PDF files are allowed'],
+            ]);
+        }
     }
 }
